be57d77886e8de11946167b51a9731b61be40290
NY.MA <NY.MA@PNC.LAN>
2024-06-11 20:18:38 +0700

    logout code #14

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -1,12 +1,20 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Validation\ValidatesRequests;
 use Illuminate\Routing\Controller as BaseController;
 
+
 class Controller extends BaseController
 {
     use AuthorizesRequests, ValidatesRequests;
+    public function logout(Request $request){
+        if ($request->user()->tokens()->delete()) {
+            return response()->json(['message' => 'Successfully logout'],200);
+        }else {
+            return response()->json(['message' => 'Unable to logout'],500);
+        }
+    }
 }
